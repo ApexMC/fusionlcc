5c6d3040f7ac7d2cb56ae3c42a0d678a9b1c0758
Quantum Beauty Group <dev@quantumbeautygroup.com>
2026-08-03 15:31:52 -0500

    testing live stripe payments

--- components/account/parent_enrollments.tsx
+++ components/account/parent_enrollments.tsx
@@ -189,7 +189,6 @@ export function ParentEnrollments({
                               <Eye />
                               View
                             </Button>
-                            {/*
                             {canStartSubscription ? (
                               <Button
                                 type="button"
@@ -222,7 +221,6 @@ export function ParentEnrollments({
                                   : "Manage"}
                               </Button>
                             ) : null}
-                             */}
                             {enrollment.status === "pending" ? (
                               <Button
                                 type="button"
